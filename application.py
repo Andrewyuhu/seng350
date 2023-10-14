5c2bcfbc0da8697d1841b3a7c5be70c495dac5b4
Andrew Hu <86829107+Andrewyuhu@users.noreply.github.com>
2023-10-14 15:33:00 -0700

    Add files via upload

---
+++ application.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, render_template, request, session, redirect, url_for
+import os
+import json
+from os import environ
+
+app = Flask(__name__, static_folder='staticFiles')
+inventory = None
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+triageSymptoms = {
+  "pain_level": 1,
+  "physical_pain": False,
+  "headache": True,
+  "sore_throat": False,
+  "runny_nose": True,
+  "allergies": False,
+  "inflamation": "none",
+  "head_trauma": False
+}
+
+@app.route('/triage')
+def triage():
+    if (triageSymptoms["head_trauma"] == True):
+        return "go to ER, there is a threat to your life"
+    elif(triageSymptoms["allergies"] == True):
+        return "OTC drugs"
+    elif(triageSymptoms["inflamation"] or triageSymptoms["physical_pain"]):
+        return "Plesae visit a GP in person"
+    
+    return "Further in person triage necessary to determine results"
+    
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=4000, debug=True)
+    
